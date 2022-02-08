3c514e0d7352e9603f0191b89510fd0193221bdd
Paulosmj <psmeneguesso@gmail.com>
2022-02-08 15:39:18 -0300

    Integrando Tabela e Formulário

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,9 @@ import Tabela from '../components/Tabela';
 import Cliente from '../core/Cliente';
 
 export default function Home() {
+  const [cliente, setCliente] = useState<Cliente>(Cliente.vazio());
+  const [visivel, setVisivel] = useState<'tabela' | 'form'>('tabela');
+
   const clientes = [
     new Cliente('Ana', 34, '1'),
     new Cliente('Bia', 45, '2'),
@@ -14,7 +17,8 @@ export default function Home() {
   ];
 
   function clienteSelecionado(cliente: Cliente) {
-    console.log(cliente.nome);
+    setCliente(cliente);
+    setVisivel('form');
   }
 
   function clienteExcluido(cliente: Cliente) {
@@ -23,9 +27,13 @@ export default function Home() {
 
   function salvarCliente(cliente: Cliente) {
     console.log(cliente);
+    setVisivel('tabela');
   }
 
-  const [visivel, setVisivel] = useState<'tabela' | 'form'>('tabela');
+  function novoCliente() {
+    setCliente(Cliente.vazio());
+    setVisivel('form');
+  }
 
   return (
     <div
@@ -39,11 +47,7 @@ export default function Home() {
         {visivel === 'tabela' ? (
           <>
             <div className="flex justify-end">
-              <Botao
-                cor="green"
-                className="mb-4"
-                onClick={() => setVisivel('form')}
-              >
+              <Botao cor="green" className="mb-4" onClick={novoCliente}>
                 Novo Cliente
               </Botao>
             </div>
@@ -55,7 +59,7 @@ export default function Home() {
           </>
         ) : (
           <Formulario
-            cliente={clientes[2]}
+            cliente={cliente}
             clienteMudou={salvarCliente}
             cancelado={() => setVisivel('tabela')}
           />
